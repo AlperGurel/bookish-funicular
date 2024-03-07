259810e11df85247d919e743a8a2a00da649790e
Alper <grl.alper@gmail.com>
2024-03-07 17:22:29 +0300

    all

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,268 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
+
+const endpoint = "https://helloworld-udo4zt7ixa-uc.a.run.app/";
+
+interface ApiResponse {
+  message?: string;
+  group?: number;
+  rate_limit_left?: number;
+  stream_seq?: 0;
+  error?: boolean;
+}
 
 export default function Home() {
+  const [variableToken, setVariableToken] = useState("USER999");
+
+  const [res1, setRes1] = useState("");
+  const [res2, setRes2] = useState<ApiResponse | null>(null);
+  const [res3, setRes3] = useState<ApiResponse | null>(null);
+  const [res4, setRes4] = useState<ApiResponse[]>([]);
+  const [res5, setRes5] = useState<ApiResponse[]>([]);
+
+  const [res1Waiting, setRes1Waiting] = useState(false);
+  const [res2Waiting, setRes2Waiting] = useState(false);
+  const [res3Waiting, setRes3Waiting] = useState(false);
+  const [res4Waiting, setRes4Waiting] = useState(false);
+  const [res5Waiting, setRes5Waiting] = useState(false);
+
+  const onReq1 = async () => {
+    setRes1("");
+    setRes1Waiting(true);
+    const res = await fetch(endpoint);
+    if (res.status == 200) {
+      const data = await res.json();
+      console.log(data);
+    } else if (res.status == 401) {
+      setRes1("Unauthorized.");
+    }
+    setRes1Waiting(false);
+  };
+
+  const onReq2 = async () => {
+    setRes2(null);
+    setRes2Waiting(true);
+    const res = await fetch(endpoint, {
+      headers: {
+        authorization: "Bearer USER123",
+      },
+    });
+    if (res.status == 200) {
+      const data = await res.json();
+      setRes2(data);
+      console.log(data);
+    } else if (res.status == 401) {
+      setRes2({ message: "Unauthorized." });
+    }
+    setRes2Waiting(false);
+  };
+
+  const onReq3 = async () => {
+    setRes3(null);
+    setRes3Waiting(true);
+    const res = await fetch(endpoint, {
+      headers: {
+        authorization: `Bearer ${variableToken}`,
+      },
+    });
+    if (res.status == 200) {
+      const data = await res.json();
+      setRes3(data);
+      console.log(data);
+    } else if (res.status == 401) {
+      setRes3({ message: "Unauthorized." });
+    }
+    setRes3Waiting(false);
+  };
+
+  const onReq4 = async () => {
+    setRes4([]);
+    setRes4Waiting(true);
+
+    while (true) {
+      const res = await fetch(endpoint, {
+        headers: {
+          authorization: `Bearer USER555`,
+        },
+      });
+      if (res.status == 200) {
+        const data = await res.json();
+        setRes4((prevRes) => [...prevRes, data]);
+      } else if (res.status == 429) {
+        setRes4((prevRes) => [
+          ...prevRes,
+          {
+            message: "Rate Limit Exceeded!",
+            error: true,
+          },
+        ]);
+        console.log(res4);
+        break;
+      }
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+      <div className="z-10 max-w-5xl w-full font-mono text-sm">
+        <div>
+          <h2 className="text-2xl font-bold">API Endpoint</h2>
+          <p className="mt-4">
+            API için Google Cloud Functions kullandım. Bu tercihimin nedeni daha
+            önceden Express.js ve Firestore tecrübem olması.
+          </p>
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="https://helloworld-udo4zt7ixa-uc.a.run.app/"
+            className="text-blue-600 font-bold"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            Bu link
           </a>
+          'i kullanarak API Endpoint'ine ulaşabilirsiniz.
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className="mt-4">
+          <h3 className="text-2xl font-bold mt-12">Authorization</h3>
+          <div className="mt-4">
+            <p>
+              Herhangi bir parametre vermeden bu endpoint'e istek atıldığı zaman{" "}
+              <i>stream</i> parametresi default olarak <i>false</i> değerini
+              alır.
+            </p>
+            <p>
+              Aşağıdaki istek gönder butonuna tıklayarak yada curl komutu ile
+              server'a istek gönderebilirsiniz.
+            </p>
+            <code className="bg-stone-800 text-white/70 p-4 rounded-lg inline-block mt-2">
+              curl "https://helloworld-udo4zt7ixa-uc.a.run.app/"
+            </code>
+            <button
+              className="block bg-green-400 p-2 rounded-lg mt-2"
+              onClick={onReq1}
+            >
+              İstek Gönder
+            </button>
+            <div className="bg-stone-300 p-4  mt-2 rounded-lg text-slate-700">
+              <span className="font-bold text-xs">Response</span>
+              <div className="mt-3">
+                {res1Waiting && "Yanıt bekleniyor..."}
+                {res1}
+              </div>
+            </div>
+            <p className="mt-4">
+              Herhangi bir authentication header'ı göndermediğimiz için
+              "Unauthorized." hatası aldık.
+            </p>
+            <p className="mt-4">
+              Aşağıdaki curl komutu ile gereken header'ları ekleyip tekrar
+              deneyebiliriz.
+            </p>
+            <code className="bg-stone-800 text-white/70 p-4 rounded-lg inline-block mt-2">
+              curl "https://helloworld-udo4zt7ixa-uc.a.run.app/" -H
+              "Authorization: Bearer USER123"
+            </code>
+            <button
+              className="block bg-green-400 p-2 rounded-lg mt-2"
+              onClick={onReq2}
+            >
+              İstek Gönder
+            </button>
+            <div className="bg-stone-300 p-4  mt-2 rounded-lg text-slate-700">
+              <span className="font-bold text-xs">Response</span>
+              <div className="mt-3">
+                {res2Waiting && "Yanıt bekleniyor..."}
+                {res2 && res2.message}
+              </div>
+            </div>
+            <p className="mt-8">
+              Eğer farklı tokenlar ile test etmek istersek aşağıdaki inputtaki
+              tokenı değiştirmemiz yeterli.
+            </p>
+            <input
+              className="p-2 mt-2 border-2 rounded-md"
+              type="text"
+              value={variableToken}
+              onChange={(e) => {
+                setVariableToken(e.target.value);
+              }}
+            />{" "}
+            <span>Token</span>
+            <code className="bg-stone-800 text-white/70 p-4 rounded-lg block mt-2">
+              curl "https://helloworld-udo4zt7ixa-uc.a.run.app/" -H
+              "Authorization: Bearer {variableToken?.toUpperCase()}"
+            </code>
+            <button
+              className="block bg-green-400 p-2 rounded-lg mt-2"
+              onClick={onReq3}
+            >
+              İstek Gönder
+            </button>
+            <div className="bg-stone-300 p-4  mt-2 rounded-lg text-slate-700">
+              <span className="font-bold text-xs">Response</span>
+              <div className="mt-3">
+                {res3Waiting && "Yanıt bekleniyor..."}
+                {res3 && res3.message}
+              </div>
+            </div>
+            <p className="mt-4">
+              <span className="font-bold text-lg"># </span>
+              User'lar için ziyaret sayısı FireStore'a kaydediliyor.
+            </p>
+            <p className="mt-4">
+              <span className="font-bold text-lg"># </span>
+              "USER_ASDF" gibi standart dışı bir token kullanıldığı zaman yine
+              "Unauthorized" hatası alıyoruz.
+            </p>
+          </div>
+        </div>
+        <div className="mt-4">
+          <h3 className="text-2xl font-bold mt-12">Rate Limiting</h3>
+          <p className="mt-2">Mevcut limit: 4 request/dakika</p>
+          <p>
+            Kullanıcılar bu limiti aştıklarında "429 - Rate Limit Exceeded"
+            hatası alıyorlar.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className="font-bold">
+            Aşağıdaki butona bir kez tıklayarak senaryonun çıktısını
+            görebilirsiniz.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+          <code className="bg-stone-800 text-white/70 p-4 rounded-lg inline-block mt-2">
+            curl "https://helloworld-udo4zt7ixa-uc.a.run.app/" -H
+            "Authorization: Bearer USER555"
+          </code>
+          <button
+            className="block bg-green-400 p-2 rounded-lg mt-2 disabled:bg-slate-300"
+            onClick={onReq4}
+            disabled={res4Waiting}
+          >
+            Rate Limit Senaryosunu Başlat
+          </button>
+          <div className="bg-stone-300 p-4  mt-2 rounded-lg text-slate-700">
+            <span className="font-bold text-xs">Response</span>
+            <div className="mt-3">
+              {res4Waiting && "Yanıt bekleniyor..."}
+              {res4.map((el, index) => (
+                <div key={index}>
+                  {el.error && <p className="text-red-500">{el?.message}</p>}
+                  {!el.error && <p className="">{el?.message}</p>}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="mt-4">
+          <h3 className="text-2xl font-bold mt-12">Streaming</h3>
+          <code className="bg-stone-800 text-white/70 p-4 rounded-lg inline-block mt-2">
+            curl "https://helloworld-udo4zt7ixa-uc.a.run.app?stream=true" -H
+            "Authorization: Bearer USER389" --no-buffer
+          </code>
+          <p>
+            <i>stream</i> parametresini <i>true</i> yaptığımız durumda
+            server'dan 5 parça halinde response gelecektir.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <p>Yukarıdaki curl komutu ile bunu görebilirsiniz.</p>
+        </div>
       </div>
     </main>
   );
